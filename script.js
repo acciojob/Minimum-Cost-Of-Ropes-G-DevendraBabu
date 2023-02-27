5068cadd078472cd5b034871fcd287f03298d882
G-DevendraBabu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-02-27 14:31:36 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function calculateMinCost() {
   //your code here
+  // console.log("hello im working");
+  let inp_elements=document.getElementById("rope-lengths").value;
   
-  
-  
+  let fil=inp_elements.split(",");
+  console.log(fil[2]);
+  var num_array=[];
+  for(let i=0;i<fil.length;i++)
+  {
+    let a=parseInt(fil[i]);
+    num_array.push(a);
+  }
+
+  // var sum=0;
+//  num_array.sort();
+  console.log(typeof num_array[1]);
+  console.log(num_array.sort());
+  // console.log(fil);
 }  
